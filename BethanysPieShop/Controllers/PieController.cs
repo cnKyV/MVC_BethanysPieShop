1d2b733aa62d35cc85a6257161f5af87845ab853
Cenkay Vergili <31133723+cnKyV@users.noreply.github.com>
2020-08-25 21:26:58 +0300

    Added controllers, models and viewmodels

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            var piestlwmodel = new PiesListViewModel();
+            piestlwmodel.CurrentCategory = "Cheese cakes";
+            piestlwmodel.Pies = _pieRepository.AllPies;
+
+            return View(piestlwmodel);
+        }
+    }
+}
--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -11,6 +11,13 @@ namespace BethanysPieShop.Models
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
-        public string AllergyInformation { get; set; }
+        public string AllergyInformat{ get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
