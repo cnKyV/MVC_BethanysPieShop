c1f749cc65d37f10e648872d7d377a4034ae39f1
Cenkay Vergili <31133723+cnKyV@users.noreply.github.com>
2020-08-25 16:18:36 +0300

    Add project files.

---
+++ BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string AllergyInformation { get; set; }
+    }
+}
