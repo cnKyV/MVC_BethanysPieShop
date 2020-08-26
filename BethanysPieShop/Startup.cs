2929573d8e3a2d01a56f8e4f0b26c1980482b97e
Cenkay Vergili <31133723+cnKyV@users.noreply.github.com>
2020-08-26 15:31:45 +0300

    DbContext and related migrations are now added

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,14 +15,20 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPieRepository, MockPieRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
-                
+            services.AddDbContext<AppDbContext>(dbcontext => 
+            dbcontext.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
