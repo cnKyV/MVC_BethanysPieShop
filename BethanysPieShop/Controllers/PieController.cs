e95b44ef3d4c7e8061e5cb940ff40fa048e11f15
Cenkay Vergili <31133723+cnKyV@users.noreply.github.com>
2020-08-27 15:14:19 +0300

    Components and custom tag halpers are now added

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -17,13 +17,28 @@ namespace BethanysPieShop.Controllers
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            var piestlwmodel = new PiesListViewModel();
-            piestlwmodel.CurrentCategory = "Cheese cakes";
-            piestlwmodel.Pies = _pieRepository.AllPies;
+            IEnumerable<Pie> pies;
+            string currentCategory;
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All Pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
 
-            return View(piestlwmodel);
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public ActionResult Details(int id)
